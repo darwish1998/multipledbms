59beef088a16f314e8f985491c9ffdfe997d69c7
Ahmed <darwish144.ad@gmail.com>
2022-03-19 15:46:51 +0300

    3 differents databases added completely

--- school/views.py
+++ school/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from school.forms import SchoolForm
+from .models import School
+from .forms  import SchoolForm
+
 
 # Create your views here.
+
+def school(request):
+    school = School.objects.all() 
+    if request.method  == 'POST':
+        form = SchoolForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = SchoolForm()
+    context = {
+        'school': school,
+        'form': form
+
+    }
+    return render(request, 'School/school.html',context)
